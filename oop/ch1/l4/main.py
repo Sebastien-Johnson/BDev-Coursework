f51de957c848da093a275a29b3fc99518fbfaaf7
Sebastien Johnson <sebastien.z.johnson@gmail.com>
2025-04-20 16:24:38 -0400

    Added ch2, completed l1

--- oop/ch1/l4/main.py
+++ oop/ch1/l4/main.py
@@ -1,6 +1,5 @@
 def get_soldier_dps(soldier):
-    soldier_dps = soldier["damage"] * soldier["attacks_per_second"]
-    return soldier_dps
+    return soldier["damage"] * soldier["attacks_per_second"]
 
 def fight_soldiers(soldier_one, soldier_two):
     if get_soldier_dps(soldier_one) > get_soldier_dps(soldier_two):
